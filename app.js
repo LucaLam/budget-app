53398914fe53cfdefae9a1bf8776098d52d7c3c2
Luca Lamanna <luca.lamanna@hotmail.com>
2020-04-15 21:04:25 -0400

    added function constructors for expenses and incomes as well as data storage variable in the Budget Controller module

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 //BUDGET CONTROLLER
 const budgetController = (function(){
 
+    const Expense = function(id, description, value){
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
+
+    const Income = function(id, description, value){
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 
 })();
@@ -36,31 +58,40 @@ return{
 //GLOBAL APP CONTROLLER
 const controller = (function(budgetCtrl, UIctrl){
 
+    const setupEventListeners = function(){
     //This gets the information in the UI controller = DOMStrings object
     const DOM = UIctrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+        }
+        });
+
+    };
 
     //We built this function to stay DRY - it is called in both keypress and event listener below.
     const ctrlAddItem = function(){
 
     //1. get input data
         let input = UIctrl.getInput();
-        console.log(input);
 
     //2. add the item to the budget controller
     //3. add the item to the UI
     //4, calculate the budget
     //5. display the budget on the UI
 
-    }
+    };
 
-document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-document.addEventListener('keypress', function(event){
-    if(event.keyCode === 13 || event.which === 13){
-        ctrlAddItem();
+    return {
+        init: function(){
+            setupEventListeners();
+        }
     }
-});
 
 })(budgetController, UIController);
-//We do this so this module can access the other modules code.
+//We do the above code so this module can access the other modules code.
 
+controller.init();
 
